c64298cf42f6ee17a02432c58bfb525ace657e10
Alex Russell <alexrusselldev@gmail.com>
2022-05-14 14:08:44 +0100

    feat: create home layout and add to landing page

---
+++ src/layouts/home/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import { Topbar } from '../../components/topbar'
+
+export const Home = () => {
+    return (
+        <div className="home-container">
+            <Topbar />
+        </div>
+    )
+}
