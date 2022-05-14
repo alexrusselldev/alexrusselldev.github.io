36364d2a7f5e5a13cae72183c4b62288968eb12b
Alex Russell <alexrusselldev@gmail.com>
2022-05-14 14:08:22 +0100

    feat: create and style topbar component

---
+++ src/components/topbar/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import './styles.css'
+
+export const Topbar = () => {
+    return (
+        <div className="topbar-body">
+            <h1>alex russell</h1>
+        </div>
+    )
+}
