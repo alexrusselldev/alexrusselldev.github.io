44ed9a05e6c78e1036725e6907e38a2401fb352c
Alex Russell <alexrusselldev@gmail.com>
2022-05-14 17:20:32 +0100

    feat: add emblem component

---
+++ src/components/emblem/index.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react'
+import { EmblemProps } from './types'
+import './styles.css'
+
+export const Emblem = (props: EmblemProps) => {
+    const title = props.title || 'alex russell'
+    const tagline = Array.from(props.title || 'full stack developer')
+    const className = `tagline ${props.size || 'normal'}`
+
+    return (
+        <div className={className}>
+            <h1>{title}</h1>
+            <div className="tagline-container">
+                {tagline.map((char) => {
+                    return (
+                        <p className="tagline-char" key={char}>
+                            {char}
+                        </p>
+                    )
+                })}
+            </div>
+        </div>
+    )
+}
---
+++ src/components/emblem/types.ts
@@ -0,0 +1,6 @@
+import StyleProps from '../../types/props/style'
+
+export interface EmblemProps extends StyleProps {
+    title?: string
+    tagline?: string
+}
