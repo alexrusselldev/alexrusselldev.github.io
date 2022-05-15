91d54c5f692b2d98aa87de0a1efb259eaf499594
Alex Russell <alexrusselldev@gmail.com>
2022-05-15 17:44:12 +0100

    feat: add basic desktop portfolio layout

--- src/layouts/portfolio/index.tsx
+++ src/layouts/portfolio/index.tsx
@@ -1,5 +1,12 @@
 import * as React from 'react'
+import { Topbar } from '../../components/topbar'
+import './styles.css'
 
 export const Portfolio = () => {
-    return <div className="portfolio-container"></div>
+    return (
+        <div className="portfolio-container">
+            <Topbar />
+            <div className="card-container"></div>
+        </div>
+    )
 }
