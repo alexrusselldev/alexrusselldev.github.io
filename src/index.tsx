6bc6b70b2fbdcf5c97fde1be43b7684774f996bd
Alex Russell <alexrusselldev@gmail.com>
2022-05-15 09:53:31 +0100

    feat: add wip route for developing wip layout

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import './index.css'
 import reportWebVitals from './reportWebVitals'
 import { Home } from './layouts/home'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { WIP } from './layouts/WIP'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
@@ -12,6 +13,7 @@ root.render(
         <BrowserRouter>
             <Routes>
                 <Route index element={<Home />} />
+                <Route path="/wip" element={<WIP />} />
             </Routes>
         </BrowserRouter>
     </React.StrictMode>
